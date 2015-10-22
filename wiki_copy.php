93aa6a6e7b1c49f947a9bb4a3fa52c5088666b06
AaronGBoswell <AaronBoswell@MacBook-Pro.local>
2015-10-21 21:35:47 -0400

    added create file

--- wiki_copy.php
+++ wiki_copy.php
@@ -1,18 +1,3 @@
 <?php
-echo 'letssee';
-
 require 'connect.php';
-require 'simple_html_dom.php';
-$id = htmlspecialchars($_GET["ID"])
-if ($search = $db->prepare("SELECT href FROM links WHERE id = ?;")) {
-	$search->bind_param('i',$id);
-}
-$r = $search->execute();
-$search->bind_results($href);
-while($search->fetch()){
-	echo $href;
-}
-$search->close();
-
-}
 ?>
