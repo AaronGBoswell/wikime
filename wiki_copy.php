51cffa7160c08086de8afc6b137f5f194adbc7b6
AaronGBoswell <AaronBoswell@MacBook-Pro.local>
2015-10-21 21:53:01 -0400

    added create file

--- wiki_copy.php
+++ wiki_copy.php
@@ -1,15 +1,13 @@
 <?php
 require 'connect.php';
 $id = htmlspecialchars($_GET["ID"]);
-echo 'searching: '.$id;
-$search = $db->prepare("SELECT href FROM links WHERE ID = 1;");
-echo 'searched';
+if ($search = $db->prepare("SELECT href FROM links WHERE ID = ?;")) {
+	$search->bind_param('i',$id);
+}
 $r = $search->execute();
-echo 'results:'. $search->num_rows;
 $search->bind_result($href);
 while($search->fetch()){
 	echo $href;
 }
-echo 'printed results';
 $search->close();
 ?>
