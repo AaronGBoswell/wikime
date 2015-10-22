7c1e5a2ca1927e008f42d82a0ef30d499c902340
AaronGBoswell <AaronBoswell@MacBook-Pro.local>
2015-10-21 20:57:59 -0400

    added create file

--- wiki.php
+++ wiki.php
@@ -8,7 +8,6 @@ foreach(range(1,2) as $i){
     echo $i*25;
     echo $Url . '<br>';
     $html = file_get_html($Url); 
-	echo $html;
 
     foreach($html->find('a') as $element){
         $link =  $element->href;
@@ -16,28 +15,20 @@ foreach(range(1,2) as $i){
         if(strpos($link,'wikipedia.org/wiki/')){
             if(strpos($link,'m.wikipedia') === false)
                 $link = substr_replace($link,'.m',strpos($link,'.wikipedia'),0);
-            echo $link;
-            echo "1";
             if ($search = $db->prepare("SELECT * FROM links WHERE href = ?;")) {
                 $search->bind_param('s',$link);
             }
-          	echo "2";
 
             $result = $search->execute();
-            echo "3";
 
             $search->store_result();
-            echo "4";
 
             if (!$result) {
                 throw new Exception($db->error);
             }
-            echo "5";
 
             if( $search->num_rows == 0 ){
-            echo "6";
                 //mysqli_report(MYSQLI_REPORT_ALL);
-echo "7";
                 if ($insert = $db->prepare("INSERT INTO links (ID, href, count, timeSpent) values (NULL,?,0,0);")) {
     
                     $insert->bind_param('s',$link);
@@ -45,17 +36,13 @@ echo "7";
                     echo '<br> prep failedd <br>';
     
                 }
-                echo "8";
                 $result = $insert->execute();
-                echo "9";
                 $insert->store_result();
-    echo "10";
                 if (!$result) {
                     throw new Exception($db->error);
                 }else{
                     echo 'inserted <br>';
                 }
-                 echo "20";  
     
             }else{
                 echo 'already found <br>';
