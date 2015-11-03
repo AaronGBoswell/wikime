0209babf9041bf24eecdf95b87806473f0a4e1be
AaronGBoswell <AaronBoswell@MacBook-Pro.local>
2015-11-03 16:32:06 -0500

    added create file

--- getMaxID.php
+++ getMaxID.php
@@ -1,6 +1,6 @@
 <?php
 require 'silentconnect.php';
-$search = $db->prepare("SELECT * FROM links");
+$search = $db->prepare("SELECT * FROM interestingLinks");
 $r = $search->execute();
 $search->store_result();
 echo $search->num_rows;
